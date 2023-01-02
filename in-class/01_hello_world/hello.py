4665fdd0f66c0cbe48a869a82faa665e0b6614e5
Preston Carman <preston.carman@wallawalla.edu>
2023-01-02 09:41:15 -0800

    Day 1 in-class activity

---
+++ in-class/01_hello_world/hello.py
@@ -0,0 +1,10 @@
+"""
+In-Class Exercise:  A "hello world" program.
+
+File Name: hello.py
+Name:      ?
+Course:    CPTR 142
+"""
+
+# print out some text (replace the question mark with your name)
+print("Hello World! My name is ? and this may be my first program in CPTR 142!")
