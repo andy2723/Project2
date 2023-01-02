c4aa050375f86945fe7d11e50e6403508616d566
gutian <gutian@cs.wallawalla.edu>
2023-01-02 19:43:24 +0000

    Commit in-class/01_hello_world

--- in-class/01_hello_world/hello.py
+++ in-class/01_hello_world/hello.py
@@ -7,4 +7,4 @@ Course:    CPTR 142
 """
 
 # print out some text (replace the question mark with your name)
-print("Hello World! My name is ? and this may be my first program in CPTR 142!")
+print("Hello World! My name is Andy and this may be my first program in CPTR 142!")
