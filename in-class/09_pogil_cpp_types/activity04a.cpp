87d234e196a5d1b7f6c03ee4acf32a80949aa38a
Preston Carman <preston.carman@wallawalla.edu>
2023-01-16 13:59:44 -0800

    Adding pogil for week 3

---
+++ in-class/09_pogil_cpp_types/activity04a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int a;
+  double b;
+  std::cout << "First number: ";
+  std::cin >> a;
+  std::cout << "Second number: ";
+  std::cin >> b;
+  std::cout << "Quotient: " << a << "/" << b << " = " << a / b << std::endl;
+}
---
+++ in-class/09_pogil_cpp_types/activity04c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  // assign two character variables
+  char charOne = ' ';
+  char charTwo = ' ';
+
+  // print out their "sum"
+  char sum = charOne + charTwo;
+  std::cout << "sum = " << sum << std::endl;
+
+  return 0;
+}
