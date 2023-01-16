d6250b0fe3ae47e197eea408b39c4fc7c6073598
gutian <gutian@cs.wallawalla.edu>
2023-01-16 05:54:20 +0000

    Commit homework/objective_02

--- homework/objective_02/2pt_organism_class/test_herbivorous.py
+++ homework/objective_02/2pt_organism_class/test_herbivorous.py
@@ -24,5 +24,6 @@ class Test_Herbivorous(unittest.TestCase):
 
         self.assertTrue(callable(getattr(herbivorous, "feed", None)))
 
+        self.assertEqual(herbivorous.unicellular, False)        
         self.assertEqual(herbivorous.multicellular, True)
-        self.assertEqual(herbivorous.unicellular, False)
+        # self.assertEqual(herbivorous.feed(), "I am a herbivorous species meaning I do not eat meat.")
