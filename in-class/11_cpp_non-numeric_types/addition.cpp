573dca6a4a193a94939ae8976c47672c38025a12
Preston Carman <preston.carman@wallawalla.edu>
2023-01-19 11:15:20 -0800

    Adding in-class activity for day 11

---
+++ in-class/11_cpp_non-numeric_types/addition.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+ *
+ * In-Class Demonstration: Generate a random addition problem with a given
+ *                         number of digits
+ *
+ * File Name: addition.cpp
+ * Course:    CPTR 142
+ *
+ */
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+int main() {
+
+  // define variables
+
+  // prompt for and set seed
+
+  // prompt for and get number of digits
+
+  // generate random numbers
+
+  // print out problem
+
+  return 0;
+}
