32d2782d89792be58911c92d62dce502b4f839f0
gutian <gutian@cs.wallawalla.edu>
2023-01-20 19:24:24 +0000

    Commit homework/objective_03

--- homework/objective_03/1pt_pets/pet.cpp
+++ homework/objective_03/1pt_pets/pet.cpp
@@ -13,10 +13,15 @@
 
 int main() {
   // define your variables below
-
+  std::string dog_name;
+  std::string dog_type;
   // gather user input using std::cin << variableName
-
+  std::cout << "Enter your pet's name: ";
+  std::cin >> dog_name;
+  std::cout << "Enter type of animal: ";
+  std::cin >> dog_type;
   // print out the text
-
+  std::cout << "Your pet's name is " << dog_name << " and is a " << dog_type
+            << "." << std::endl;
   return 0;
 }
