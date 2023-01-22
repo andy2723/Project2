cb58b3a4477f7961236642cc2bcf875c73fb8dc4
gutian <gutian@cs.wallawalla.edu>
2023-01-22 22:04:54 +0000

    Commit homework/objective_03

--- homework/objective_03/1pt_sale/sale.cpp
+++ homework/objective_03/1pt_sale/sale.cpp
@@ -18,11 +18,11 @@ int main() {
   const double SALE_DISCOUNT = 0.15;
   double item_price;
   // gather user input using std::cin << variableName
-  cout << "Enter the item price $";
+  cout << "Enter a item price: $";
   cin >> item_price;
   // print out the text
   double sale_price = item_price - (item_price * SALE_DISCOUNT);
-  cout << "The sale price of the item is $" << sale_price << endl;
+  cout << "The sale price is $" << sale_price << "." << endl;
 
   return 0;
 }
