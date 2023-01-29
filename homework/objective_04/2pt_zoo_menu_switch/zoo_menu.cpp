43a57145033e1c3013f413fe5b247d80b654d0d5
gutian <gutian@cs.wallawalla.edu>
2023-01-29 23:24:23 +0000

    Commit homework/objective_04

--- homework/objective_04/2pt_zoo_menu_switch/zoo_menu.cpp
+++ homework/objective_04/2pt_zoo_menu_switch/zoo_menu.cpp
@@ -17,42 +17,43 @@
 //   return 0;
 // }
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 int main() {
-    char ticket_type;
-    int ticket_count;
-    double ticket_price;
-    double total_cost;
-    
-    std::cout << "What type of zoo ticket: \n";
-    std::cout << " c - Child  $10 \n";
-    std::cout << " a - Adult  $15 \n";
-    std::cout << " s - Senior $12 \n";
-    std::cin >> ticket_type;
-    
-    switch (ticket_type) {
-        case 's':
-            ticket_price = 12.00;
-            break;
-        case 'a':
-            ticket_price = 15.00;
-            break;
-        case 'c':
-            ticket_price = 10.00;
-            break;
-        default:
-            std::cout << "Invalid ticket selection." << std::endl;
-            return 0;
-    }
-    
-    std::cout << "How many tickets? \n";
-    std::cin >> ticket_count;
-    
-    total_cost = ticket_price * ticket_count;
-    
-    std::cout << "The cost will be " << std::fixed << std::setprecision(0) << total_cost << std::endl;
-    
+  char ticket_type;
+  int ticket_count;
+  double ticket_price;
+  double total_cost;
+
+  std::cout << "What type of zoo ticket: \n";
+  std::cout << " c - Child  $10 \n";
+  std::cout << " a - Adult  $15 \n";
+  std::cout << " s - Senior $12 \n";
+  std::cin >> ticket_type;
+
+  switch (ticket_type) {
+  case 's':
+    ticket_price = 12.00;
+    break;
+  case 'a':
+    ticket_price = 15.00;
+    break;
+  case 'c':
+    ticket_price = 10.00;
+    break;
+  default:
+    std::cout << "Invalid ticket selection." << std::endl;
     return 0;
+  }
+
+  std::cout << "How many tickets? \n";
+  std::cin >> ticket_count;
+
+  total_cost = ticket_price * ticket_count;
+
+  std::cout << "The cost will be " << std::fixed << std::setprecision(0)
+            << total_cost << std::endl;
+
+  return 0;
 }
