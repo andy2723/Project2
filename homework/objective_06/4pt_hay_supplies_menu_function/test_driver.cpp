2f41267de07c6b42d805edbb4fee90a64db07f42
Preston Carman <preston.carman@wallawalla.edu>
2023-02-12 12:20:23 -0800

    Adding expert problem for objective 6

---
+++ homework/objective_06/4pt_hay_supplies_menu_function/test_driver.cpp
@@ -0,0 +1,11 @@
+/**
+ * Use this file to test your function.
+ * The code you write in this file will not be graded.
+ */
+#include "menu.h"
+#include <iostream>
+
+int main() {
+  menu();
+  return 0;
+}
