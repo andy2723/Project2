84bccaac3998d9d8ba8f474fe64cb73f47c70bbf
gutian <gutian@cs.wallawalla.edu>
2023-02-13 07:32:22 +0000

    Commit homework/objective_06

--- homework/objective_06/1pt_lock_up_prototype/lock_up.h
+++ homework/objective_06/1pt_lock_up_prototype/lock_up.h
@@ -1,10 +1,12 @@
 /**
  * Beginner: Write down the function prototypes for lock_up.cpp.
  */
+#include <iostream>
+#include <vector>
 #ifndef LOCK_UP_H
 #define LOCK_UP_H
 
-void lock();
-void unlock();
+void lock(std::vector<bool> &doors, int door);
+void unlock(std::vector<bool> &doors, int door);
 
 #endif
