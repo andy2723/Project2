131d0add4a61b0087c7fb3adeb619d5acf60b28f
gutian <gutian@cs.wallawalla.edu>
2023-02-13 07:03:12 +0000

    Commit homework/objective_06

--- homework/objective_06/1pt_food_supplies_call_reference/one_day.cpp
+++ homework/objective_06/1pt_food_supplies_call_reference/one_day.cpp
@@ -4,26 +4,14 @@
 #include "food_supplies.h"
 
 // TODO Add your code here
+#include "food_supplies.h"
 #include <iostream>
 
-void updateFoodSupplies(int &hay, int &grain, int &water, int numAnimals) {
-    hay -= numAnimals * 2;
-    grain -= numAnimals * 3;
-    water -= numAnimals * 5;
-}
-
-void printFoodSupplies(int hay, int grain, int water) {
-    std::cout << "Hay: " << hay << std::endl;
-    std::cout << "Grain: " << grain << std::endl;
-    std::cout << "Water: " << water << std::endl;
-}
-
 void oneDay(int numAnimals) {
-    int hay = 50;
-    int grain = 50;
-    int water = 50;
+  double hay = 50;
+  double grain = 50;
+  double water = 50;
 
-
-    updateFoodSupplies(hay, grain, water, numAnimals);
-    printFoodSupplies(hay, grain, water);
+  updateFoodSupplies(numAnimals, hay, grain, water);
+  printFoodSupplies(hay, grain, water);
 }
