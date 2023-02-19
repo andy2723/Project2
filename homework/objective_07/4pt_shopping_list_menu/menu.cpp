dcea5bcbc79e2f61f1069616e3e7cabfc21a2ffd
gutian <gutian@cs.wallawalla.edu>
2023-02-19 20:41:40 +0000

    Commit homework/objective_07

--- homework/objective_07/4pt_shopping_list_menu/menu.cpp
+++ homework/objective_07/4pt_shopping_list_menu/menu.cpp
@@ -7,3 +7,41 @@
 #include <iostream>
 
 // TODO Add your code here
+#include <iostream>
+#include "menu.h"
+
+void menu() {
+  int choice;
+
+  do {
+    std::cout << "========== MENU ==========" << std::endl;
+    std::cout << "1) View All Items" << std::endl;
+    std::cout << "2) Add An Item" << std::endl;
+    std::cout << "3) Remove An Item" << std::endl;
+
+    std::cout << "Enter your choice (1-3): ";
+    std::cin >> choice;
+
+    switch (choice) {
+      case 1:
+        std::cout << "Viewing all items..." << std::endl;
+        // TODO: Implement code to view all items
+        break;
+
+      case 2:
+        std::cout << "Adding an item..." << std::endl;
+        // TODO: Implement code to add an item
+        break;
+
+      case 3:
+        std::cout << "Removing an item..." << std::endl;
+        // TODO: Implement code to remove an item
+        break;
+
+      default:
+        std::cout << "Invalid choice. Please enter a number between 1 and 3." << std::endl;
+        break;
+    }
+
+  } while (choice != 0);
+}
