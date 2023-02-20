d5f08d075645cf2eda932e8ec0c0d4059a2825ca
gutian <gutian@cs.wallawalla.edu>
2023-02-20 03:03:38 +0000

    Commit homework/objective_07

--- homework/objective_07/1pt_enclosure_class_constructor/enclosure_constuctor.cpp
+++ homework/objective_07/1pt_enclosure_class_constructor/enclosure_constuctor.cpp
@@ -2,6 +2,15 @@
  * Beginner: Create a class constructor.
  */
 #include "enclosure.h"
+#include <iostream>
 #include <string>
-
 // TODO Add your code here
+
+Enclosure::Enclosure(std::string type, int size) : type(type), size(size)
+{    
+}
+
+void Enclosure::printDetails() {
+  std::cout << "The " << type << " enclosure is " << size << " square feet."
+            << std::endl;
+}
