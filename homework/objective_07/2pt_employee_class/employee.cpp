04a9f8910825bed08f962aec9a47f42eee3b50b3
gutian <gutian@cs.wallawalla.edu>
2023-02-20 02:32:13 +0000

    Commit homework/objective_07

--- homework/objective_07/2pt_employee_class/employee.cpp
+++ homework/objective_07/2pt_employee_class/employee.cpp
@@ -4,3 +4,14 @@
 #include "employee.h"
 
 // Your code goes here
+
+Employee::Employee(const std::string& name, double wage)
+    : m_name(name), m_wage(wage) {}
+
+double Employee::getPay(double hoursWorked) const {
+    return m_wage * hoursWorked;
+}
+
+std::string Employee::getName() const {
+    return m_name;
+}
