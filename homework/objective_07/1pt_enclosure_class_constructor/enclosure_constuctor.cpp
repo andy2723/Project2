c2d47cfee4eebd697c3d6a06c3fbb35d2319d885
gutian <gutian@cs.wallawalla.edu>
2023-02-20 03:18:10 +0000

    Commit homework/objective_07

--- homework/objective_07/1pt_enclosure_class_constructor/enclosure_constuctor.cpp
+++ homework/objective_07/1pt_enclosure_class_constructor/enclosure_constuctor.cpp
@@ -7,8 +7,3 @@
 // TODO Add your code here
 
 Enclosure::Enclosure(std::string type, int size) : type(type), size(size) {}
-
-void Enclosure::printDetails() {
-  std::cout << "The " << type << " enclosure is " << size << " square feet."
-            << std::endl;
-}
