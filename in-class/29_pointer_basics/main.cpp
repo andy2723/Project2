133918721a52060bc7006e3b64cd5c09045a4f98
nsmit106 <natalie.j.smith@wsu.edu>
2023-02-22 10:01:02 -0800

    Adding lecture 29 in class work.

---
+++ in-class/29_pointer_basics/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * In-class Activity: Pointers and Objects
+ */
+#include "bank_account.h"
+#include <iostream> // for cin and cout
+using namespace std;
+
+/**====================================================================
+ * Main program
+ *
+ * g++ main.cpp bank_account.cpp && ./a.out
+ *
+ */
+int main() {
+  // 1. Define a variable to hold a BankAccount object
+
+  // 2. Define a variable to hold a pointer to a BankAccount object (properly
+  //    initialized)
+
+  // 3. Assign the address of the first variable to the second
+
+  // 4. Call displayAccountSummary() using the dot operator
+
+  // 5. Call updateInterest() using the dereference operator
+
+  // 6. Call displayAccountSummary() using the arrow operator
+
+  return 0;
+}
