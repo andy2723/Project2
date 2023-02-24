f3169a82bdc82fbb2ff30c58e91130fa3c1a18f0
gutian <gutian@cs.wallawalla.edu>
2023-02-24 19:17:24 +0000

    Fixing Files

---
+++ homework/objective_07/1pt_enclosure_class_constructor/enclosure_constructor.cpp
@@ -0,0 +1,8 @@
+/**
+ * Beginner: Create a class constructor.
+ */
+#include "enclosure.h"
+#include <string>
+// TODO Add your code here
+
+Enclosure::Enclosure(std::string type, int size) : type(type), size(size) {}
