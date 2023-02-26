2c09843386267546b932450c9ee6b4952e4498c2
gutian <gutian@cs.wallawalla.edu>
2023-02-26 19:08:03 +0000

    Commit homework/objective_08

--- homework/objective_08/2pt_car_class/car.cpp
+++ homework/objective_08/2pt_car_class/car.cpp
@@ -2,5 +2,13 @@
  * Intermediate: Add the car method definitions for fillTank, drive and getGas.
  */
 #include "car.h"
+#include <cmath>
 
-// Your code goes here
+void Car::fillTank(double newGas) { *gas += newGas; }
+
+void Car::drive(double miles) {
+  double gallonsUsed = miles / 20.0;
+  *gas -= gallonsUsed;
+}
+
+double Car::getGas() { return *gas; }
