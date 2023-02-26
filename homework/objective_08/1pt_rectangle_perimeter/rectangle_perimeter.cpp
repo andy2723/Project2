71377e310998186c693de2442b8925794df7860d
gutian <gutian@cs.wallawalla.edu>
2023-02-26 18:43:05 +0000

    Commit homework/objective_08

--- homework/objective_08/1pt_rectangle_perimeter/rectangle_perimeter.cpp
+++ homework/objective_08/1pt_rectangle_perimeter/rectangle_perimeter.cpp
@@ -4,3 +4,8 @@
 #include "rectangle_perimeter.h"
 
 // Your code goes here
+int rectanglePerimeter(int *widthPtr, int *heightPtr) {
+  int width = *widthPtr;
+  int height = *heightPtr;
+  return 2 * (width + height);
+}
