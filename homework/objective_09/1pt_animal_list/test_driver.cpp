62b9242bd358e8e212e85aa7b7565498739b25b2
Preston Carman <preston.carman@wallawalla.edu>
2023-02-28 20:49:25 -0800

    Adding a few objective 9 points

---
+++ homework/objective_09/1pt_animal_list/test_driver.cpp
@@ -0,0 +1,14 @@
+/**
+ * Implement your function in the file pointer_circle.cpp
+ *
+ * Use this file to test your function.
+ * The code you write in this file will not be graded.
+ */
+#include "animal_list.h"
+#include <string>
+
+int main() {
+  std::string animals[4] = {"ant eater", "badger", "cat", "dog"};
+  printAnimals(animals, 4);
+  return 0;
+}
---
+++ homework/objective_09/1pt_light_show_arthmetic/test_driver.cpp
@@ -0,0 +1,13 @@
+/**
+ * Implement your function in the file pointer_circle.cpp
+ *
+ * Use this file to test your function.
+ * The code you write in this file will not be graded.
+ */
+#include "light_show.h"
+#include <iostream>
+
+int main() {
+  std::cout << getLightCount(20) << std::endl;
+  return 0;
+}
