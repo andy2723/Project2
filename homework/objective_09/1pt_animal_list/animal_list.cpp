668e6d5dbc340adf7162cc9a4d3b57508eaa73be
gutian <gutian@cs.wallawalla.edu>
2023-03-05 20:13:31 +0000

    Commit homework/objective_09

--- homework/objective_09/1pt_animal_list/animal_list.cpp
+++ homework/objective_09/1pt_animal_list/animal_list.cpp
@@ -6,3 +6,16 @@
 #include <string>
 
 // Your code goes here
+using namespace std;
+
+void printAnimals(string arr[], int size) {
+    // Base case
+    if (size == 0) {
+        return;
+    }
+    // Recursive case
+    else {
+        printAnimals(arr, size-1);
+        cout << arr[size-1] << endl;
+    }
+}
