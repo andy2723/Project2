ffed53a75c6872ccfc3bef5d0e10c67ed964038b
gutian <gutian@cs.wallawalla.edu>
2023-03-05 23:33:52 +0000

    Commit homework/objective_09

--- homework/objective_09/2pt_fibonacci_debug/fibonacci.cpp
+++ homework/objective_09/2pt_fibonacci_debug/fibonacci.cpp
@@ -5,3 +5,12 @@
 #include <string>
 #include <iostream>
 
+int fibonacci(int n, std::string indent) {
+  std::cout << indent << n << std::endl;
+  
+  if (n <= 1) {
+    return n;
+  }
+  
+  return fibonacci(n - 1, indent + "  ") + fibonacci(n - 2, indent + "  ");
+}
--- homework/objective_09/2pt_fibonacci_debug/fibonacci.h
+++ homework/objective_09/2pt_fibonacci_debug/fibonacci.h
@@ -4,3 +4,11 @@
 #pragma once
 #include <string>
 
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+
+#include <string>
+
+int fibonacci(int n, std::string indent);
+
+#endif
