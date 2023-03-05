92d0dc8ed963de400cffca244f44e6177e82ea9a
gutian <gutian@cs.wallawalla.edu>
2023-03-05 23:34:53 +0000

    Commit homework/objective_09

--- homework/objective_09/2pt_fibonacci_debug/fibonacci.cpp
+++ homework/objective_09/2pt_fibonacci_debug/fibonacci.cpp
@@ -2,15 +2,15 @@
  * Intermediate: Arthmetic calculation using recursion.
  */
 #include "fibonacci.h"
-#include <string>
 #include <iostream>
+#include <string>
 
 int fibonacci(int n, std::string indent) {
   std::cout << indent << n << std::endl;
-  
+
   if (n <= 1) {
     return n;
   }
-  
+
   return fibonacci(n - 1, indent + "  ") + fibonacci(n - 2, indent + "  ");
 }
