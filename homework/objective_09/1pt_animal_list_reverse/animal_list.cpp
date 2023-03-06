9ea37c385246bc56fab9614c359d35f5dd987dd8
gutian <gutian@cs.wallawalla.edu>
2023-03-06 03:16:58 +0000

    Commit homework/objective_09

--- homework/objective_09/1pt_animal_list_reverse/animal_list.cpp
+++ homework/objective_09/1pt_animal_list_reverse/animal_list.cpp
@@ -6,9 +6,16 @@
 #include <string>
 
 // Your code goes here
+#include "animal_list.h"
+#include <iostream>
+
 void printAnimals(std::string animals[], int size) {
-  if (size > 0) {
-    std::cout << animals[size - 1] << std::endl;
-    printAnimals(animals, size - 1);
+  if (size == 0) {
+    return;
+  } else if (size == 1) {
+    std::cout << animals[0] << std::endl;
+  } else {
+    printAnimals(animals + 1, size - 1);
+    std::cout << animals[0] << std::endl;
   }
 }
