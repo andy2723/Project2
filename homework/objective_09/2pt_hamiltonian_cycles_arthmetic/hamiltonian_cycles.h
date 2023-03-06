7dbadc7d2069fa2c0ca74d899d65ef871e94353b
gutian <gutian@cs.wallawalla.edu>
2023-03-06 02:56:41 +0000

    Commit homework/objective_09

--- homework/objective_09/2pt_hamiltonian_cycles_arthmetic/hamiltonian_cycles.h
+++ homework/objective_09/2pt_hamiltonian_cycles_arthmetic/hamiltonian_cycles.h
@@ -2,3 +2,9 @@
  * Intermediate: Arthmetic calculation using recursion.
  */
 #pragma once
+#ifndef HAMILTONIAN_CYCLES_H
+#define HAMILTONIAN_CYCLES_H
+
+long long getHamiltonianCycles(int n);
+
+#endif // HAMILTONIAN_CYCLES_H
