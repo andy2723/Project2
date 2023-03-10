108d3eb2a2ef90eab96ff76d20614516b4bdc016
nsmit106 <natalie.j.smith@wsu.edu>
2023-03-10 09:17:58 -0800

    Added the in-class work for lecture 38.

---
+++ in-class/38_cpp_containers/blackFriday.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+ *
+ * File Name: blackFriday.cpp
+ * Course:    CPTR 142
+ *
+ */
+
+#include <deque>
+#include <iostream> // for std::cin and std::cout
+
+int main() {
+  // TODO: define a variable to hold a deque of people
+
+  // TODO: add the names Alice, Bob, Carol, David, and John (in this order)
+
+  // TODO: add Nick to the front of the line
+
+  // TODO: Move Nick and Alive to the back of the line
+
+  // TODO: Who is at the front of the line?
+
+  // How many people are in the line?
+
+  return 0;
+}
